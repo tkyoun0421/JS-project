00e9486a7ea1b8b66d88ff6c4a6139ebe43b830c
태관 <tkyoun0421@naver.com>
2023-08-01 16:22:21 +0900

    Env: 투두 앱 폴더 구조 완성

---
+++ ToDoApp/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const todoBtnAddEl = document.querySelector('.todo_btn--add')
+const todoCheckboxEl = document.querySelector('.todo_checkbox')
+const todoTextEl = document.querySelector('.todo_text')
+const todoBtnModifyEl = document.querySelector('.todo_btn--modify')
+const todoBtnDeleteEl = document.querySelector('.todo_btn--delete')
+
+todoBtnAddEl.onclick = {
+  
+}
